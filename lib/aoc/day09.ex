07d4754306c189d04dc31612913b3dacfa43ec32
Marco Delaurenti <mfd@fastmail.com>
2025-12-16 19:31:42 +0000

    Day09

---
+++ lib/aoc/day09.ex
@@ -0,0 +1,69 @@
+defmodule Aoc.Day09 do
+  def part1(args) do
+    g =
+      args
+      |> parse_input()
+      |> build_graph()
+
+    find_all_paths(g)
+    |> Enum.map(&length_path(&1, g))
+    |> Enum.min()
+  end
+
+  def part2(args) do
+    g =
+      args
+      |> parse_input()
+      |> build_graph()
+
+    find_all_paths(g)
+    |> Enum.map(&length_path(&1, g))
+    |> Enum.max()
+  end
+
+  def parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_line/1)
+  end
+
+  def parse_line(line) do
+    [nodes, distance] = String.split(line, " = ", trim: true)
+    [from, to] = String.split(nodes, " to ", trim: true)
+    {from, to, String.to_integer(distance)}
+  end
+
+  def build_graph(input) do
+    input
+    |> Enum.reduce(Graph.new(type: :undirected), fn {from, to, distance}, graph ->
+      graph
+      |> Graph.add_edge(from, to, weight: distance)
+    end)
+  end
+
+  def find_all_paths(graph) do
+    graph
+    |> Graph.vertices()
+    |> permutations()
+    |> Enum.filter(&valid_path?(&1, graph))
+  end
+
+  defp valid_path?(path, graph) do
+    path
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.all?(fn [from, to] -> Graph.edge(graph, from, to) != nil end)
+  end
+
+  def length_path(path, graph) do
+    path
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.map(fn [from, to] -> Graph.edge(graph, from, to).weight end)
+    |> Enum.sum()
+  end
+
+  defp permutations([]), do: [[]]
+
+  defp permutations(list) do
+    for h <- list, t <- permutations(list -- [h]), do: [h | t]
+  end
+end
--- lib/aoc/day09_test.ex
+++ lib/aoc/day09_test.ex
@@ -1,14 +1,13 @@
-defmodule Aoc.Day08Test do
+defmodule Aoc.Day09Test do
   use ExUnit.Case
 
-  import Elixir.Aoc.Day08
+  import Elixir.Aoc.Day09
 
   def test_input() do
     """
-      ""
-      "abc"
-      "aaa\\\"aaa"
-      "\x27"
+    London to Dublin = 464
+    London to Belfast = 518
+    Dublin to Belfast = 141
     """
   end
 
@@ -16,15 +15,13 @@ defmodule Aoc.Day08Test do
     input = test_input()
     result = part1(input)
 
-    # == 12
-    assert result
+    assert result == 605
   end
 
   test "part2" do
     input = test_input()
     result = part2(input)
 
-    # == 19
-    assert result
+    assert result == 982
   end
 end
